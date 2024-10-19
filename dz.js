83251f10ce9ffd3e8fea1628dc5f010fb2445f32
Timka09 <tymofij.i.naumenko@gmail.com>
2024-10-20 01:24:50 +0300

    hz

---
+++ dz.js
@@ -0,0 +1,20 @@
+// // // Task 1
+// let num = 1
+// while (num <= 10) {
+//     console.log(num);
+//     num ++
+// }
+// // Task 2
+// for (let i = 2; i <= 20; i += 2) {
+//     console.log(i)
+// }
+// // Task 3
+// for (let y = 7; y <= 70; y += 7) {
+//     console.log(y);
+// }
+// Task 4
+let mass = [1, 2, 3, 4, 5]
+let calc = 0
+while (  mass.length   ) {
+console.log(mass)
+}
